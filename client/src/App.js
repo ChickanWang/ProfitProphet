9e49789d771c5768942eb321d249358c7fa888ad
LouPInnovations <LouPInnovations@users.noreply.github.com>
2021-01-16 00:41:10 -0500

    set up routers for members to work on different pages

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import PhilTest from './components/PhilTest.js';
+import { Route, Switch } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Switch>
+        <Route path="/phil" component={PhilTest} exact />
+      </Switch>
     </div>
   );
 }
---
+++ client/src/components/PhilTest.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function PhilTest(){
+    
+    return (
+        <div>
+            phil test
+        </div>
+    )
+}
+
+export default PhilTest;
