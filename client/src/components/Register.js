8c1accbfa0b4e2603acb0c12e2aed8edc188d86f
Eric Wang <ericwang52@yahoo.ca>
2021-01-17 02:37:00 -0500

    fixed register

--- client/src/components/Register.js
+++ client/src/components/Register.js
@@ -1,24 +1,92 @@
-import React from 'react';
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import {LoginContext} from "./LoginContext"
 
-function Register() {
+class Register extends Component {
+  constructor() {
+      super();
+      this.state = {
 
-    return (
+        email: "",
+        password: "",
+
+        errors: {}
+      };
+
+      this.onChange = this.onChange.bind(this)
+      this.onSubmit = this.onSubmit.bind(this)
+  }
+  
+  componentDidMount(){
+      this._isMounted = true;
+  }
+  componentWillUnmount() {
+      this._isMounted = false;
+  }
+  // Form Change handler
+  onChange(e){
+      this.setState({ [e.target.id]: e.target.value });
+  };
+
+  // Create new user account from form/state data
+  onSubmit(e){
+      e.preventDefault();
+      const newUser = {
+        email: this.state.email,
+        password: this.state.password,
+
+      }
+
+      const requestOptions = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify(newUser)
+      }
+      
+      // Make request to backend to create account
+      console.log(requestOptions)
+      fetch('/api/register', requestOptions)
+      .then(response => {
+      const r = response.json()
+          if(response.ok){
+              this.props.history.push("/login");
+          }
+          return r
+      })
+      .then(data => {
+          if(this._isMounted){
+              this.setState({ errors: data })
+          }
+      })
+  };
+
+  render() {
+    // Redirect if already logged in
+    if(this.context.loggedIn){
+      this.props.history.push("/search");
+    }
+      const { errors } = this.state;
+      return (
         <div>
         <div className="form">
           <h1>Register</h1>
           <br />
           <br />
           <br />
-          <form>
+          <form onSubmit={this.onSubmit}>
             <div className="form-group textbox">
               <label htmlFor="exampleInputEmail1">Email address</label>
-              <input type="email" className="form-control inputbox" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email"/>
+              <input type="email" className="form-control inputbox" id="email" onChange={this.onChange}
+                    value={this.state.email}
+                    aria-describedby="emailHelp" placeholder="Enter email"/>
             </div>
             <br />
 
             <div class="form-group textbox">
               <label htmlFor="exampleInputPassword1">Password</label>
-              <input type="password" className="form-control inputbox" id="exampleInputPassword1" placeholder="Password"/>
+              <input type="password" onChange={this.onChange}
+                    value={this.state.password}
+                    className="form-control inputbox" id="password" placeholder="Password"/>
             </div>
             <br />
             <br />
@@ -33,7 +101,9 @@ function Register() {
           </form>
         </div>
       </div>
-    )
+      );
+    }
 }
 
+Register.contextType = LoginContext
 export default Register;
