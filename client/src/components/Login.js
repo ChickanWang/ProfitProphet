ecd6a371004a9dd9a63f8d40c6dba81e5c6b4e28
PhilbertLou <philbert.lou@uwaterloo.ca>
2021-01-17 01:15:45 -0500

    Removed navbar

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -65,27 +65,7 @@ class Login extends Component {
     const { errors } = this.state;
     return (
       <div>
-        <nav class="navbar navbar-expand-lg navbar-light bg-light">
-          <a class="navbar-brand" href="#">Profit Prophet</a>
-          <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
-          </button>
-
-            <div class="collapse navbar-collapse" id="navbarSupportedContent">
-              <ul class="navbar-nav mr-auto">
-              <li className="nav-item">
-                      <Link to="/login" className="nav-link text-muted">Login</Link>
-                  </li>
-                  <li className="nav-item">
-                      <Link to="/register" className="nav-link text-muted">Register</Link>
-                  </li>
-                  <a class="btn bluebtn">Hover me</a> 
-              </ul>
-          </div>
-        </nav>
-      <p>test</p>
-
-        <button type="button" class="btn btn-outline-info">Info</button>
+        <button type="button" class="btn btn-outline-primary loginbutton">Primary</button>
       </div>
     );
   }
