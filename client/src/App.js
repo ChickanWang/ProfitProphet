edd8849e2faf29c9b5cc0338a104ffb4d8d53ffa
Stephen Wang <stephenwang1234567890@gmail.com>
2021-01-16 21:09:35 -0500

    added firebase + flask auth (not working)

--- client/src/App.js
+++ client/src/App.js
@@ -1,12 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
 import PhilTest from './components/PhilTest.js';
-<<<<<<< HEAD
-import Homepage from './components/Homepage.js'
-=======
 import Home from './components/Home.js';
 // import SteveTest from './components/SteveTest.js'
->>>>>>> 77fa9512b7eaa26c08dad2f7c38fc3aad4ce0229
 import { Route, Switch } from 'react-router-dom';
 
 function App() {
@@ -14,12 +10,8 @@ function App() {
     <div>
       <Switch>
         <Route path="/phil" component={PhilTest} exact />
-<<<<<<< HEAD
-        <Route path="/" component={Homepage} exact />
-=======
         <Route path="/home" component={Home} exact />
         {/* <Route path="/steve" component={SteveTest} exact /> */}
->>>>>>> 77fa9512b7eaa26c08dad2f7c38fc3aad4ce0229
       </Switch>
     </div>
   );
